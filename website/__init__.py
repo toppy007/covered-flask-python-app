4438b98c3d622b6d340ae30320bebb00c91127d3
Mr C R Toplisek <mrcrtoplisek@Mrs-MacBook-Air.local>
2023-07-31 10:52:43 +0100

    added base.html page

--- website/__init__.py
+++ website/__init__.py
@@ -3,5 +3,11 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'this key needs to be hidden in production'
+    
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     return app
