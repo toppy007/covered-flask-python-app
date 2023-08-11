33fdc989205937fee222ff4c1f108887c99b2538
Mr C R Toplisek <mrcrtoplisek@Mrs-MacBook-Air.local>
2023-08-11 09:48:11 +0100

    add conditional statement around test database

--- website/__init__.py
+++ website/__init__.py
@@ -7,10 +7,14 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 
-def create_app():
+def create_app(testing=False):
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    
+    if testing:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test_' + DB_NAME
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views
@@ -33,7 +37,6 @@ def create_app():
         return User.query.get(int(id))
 
     return app
-
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
