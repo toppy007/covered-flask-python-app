73d0773f7a0f4e7b686c57500e896cfab378e914
toppy007 <chris_t72@hotmail.com>
2023-08-11 14:54:59 +0100

    all tests passing

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,15 @@ import pytest
 
 from website.models import User, Note, Skill, OpenAiApiKey
 from website import create_app, db, create_database
+from werkzeug.security import generate_password_hash
 
 @pytest.fixture
 def app():
     app = create_app(testing=True)
     with app.app_context():
-        create_database(app, testing=True)  # Create test database
-        user = User(email='testuser@example.com', first_name='Test', password='testpassword')
+        create_database(app, testing=True)
+        user = User(email='testuser@example.com', first_name='Test', password=generate_password_hash(
+                "testpassword", method='sha256'))
         db.session.add(user)
         db.session.commit()
         yield app
