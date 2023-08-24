08938333c214ae7413a9266a28f6f09869f09063
toppy007 <chris_t72@hotmail.com>
2023-08-24 13:03:24 +0100

    bug semi fixed for dict.get interaction

--- website/create_prompts.py
+++ website/create_prompts.py
@@ -4,17 +4,22 @@ from fuzzywuzzy import fuzz
 def create_matches_prompt(analysis_dict, user_id):
     skills = Skill.query.filter_by(user_id=user_id).all()
 
-    job_ad_skills = analysis_dict.get("Technical Skills", [])
+    job_ad_skills = analysis_dict.get("Technical skills", [])
     
+    print("these are my matching skills lists")
     print(job_ad_skills)
+    for skill in skills:
+        print(skill.data)
     
     matching_skills = []
     for skill in skills:
         for job_skill in job_ad_skills:
             similarity_ratio = fuzz.ratio(skill.data.lower(), job_skill.lower())
-            if similarity_ratio >= 70:  # Adjust the threshold as needed
+            if similarity_ratio >= 50:  # Adjust the threshold as needed
                 matching_skills.append(skill.data)
                 break  # Move to the next user skill
+            
+            print(matching_skills)
             
 
     system_prompt = "**How suitable am I for this role**\n\n"
