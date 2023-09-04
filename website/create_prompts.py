ea3c6bd80bac7c14e9795de0c934f12107a5f6fb
toppy007 <chris_t72@hotmail.com>
2023-09-04 20:28:24 +0100

    threshold added to include projects for covering letter

--- website/create_prompts.py
+++ website/create_prompts.py
@@ -1,7 +1,11 @@
 from .models import Skill, Project, Workexp
 
-def threshold_projects_to_include(projects, theshold): 
-    
-    projects_to_include = projects
-    
+def threshold_projects_to_include(projects_evaluation, threshold=0.2):
+    projects_to_include = []
+
+    for project_evaluation in projects_evaluation:
+        for tup in project_evaluation:
+            if tup[2] >= threshold:
+                projects_to_include.append(tup[2])
+
     return projects_to_include
