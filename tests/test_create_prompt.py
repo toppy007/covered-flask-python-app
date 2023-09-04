84a6243ca369f16f1405d67452bf3843f676b63a
toppy007 <chris_t72@hotmail.com>
2023-09-04 20:50:01 +0100

    fixed outfut of function

--- tests/test_create_prompt.py
+++ tests/test_create_prompt.py
@@ -2,10 +2,10 @@ from website.create_prompts import threshold_projects_to_include
 
 def test_threshold_projects_to_include():
     input_data = [
-        [(2, 'Brolli', 0.16776883802873355), (3, 'Covered', 0.15093333934707948), (4, 'Sentiment Analysis', 0.21527101437610116)]
+        [(2, 'Brolli', 0.26776883802873355), (3, 'Covered', 0.15093333934707948), (4, 'Sentiment Analysis', 0.21527101437610116)]
     ]
     expected_output = [
-        (4)
+       2, 4
     ]
 
     result = threshold_projects_to_include(input_data)
--- website/create_prompts.py
+++ website/create_prompts.py
@@ -6,6 +6,11 @@ def threshold_projects_to_include(projects_evaluation, threshold=0.2):
     for project_evaluation in projects_evaluation:
         for tup in project_evaluation:
             if tup[2] >= threshold:
-                projects_to_include.append(tup[2])
+                projects_to_include.append(tup[0])
 
     return projects_to_include
+
+def theshold_projects_db_queary(project_ids):
+    projects = Project.query.filter(Project.id.in_(project_ids)).all()
+    
+    return projects
