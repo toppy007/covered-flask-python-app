c16007a33b550ca0dc4915f0c96a1f498c0bcbf9
toppy007 <chris_t72@hotmail.com>
2023-09-04 19:58:32 +0100

    reworking new prompt system built on matching projects and work experience

---
+++ website/create_prompts.py
@@ -0,0 +1,7 @@
+from .models import Skill, Project, Workexp
+
+def threshold_projects_to_include(projects, theshold): 
+    
+    projects_to_include = projects
+    
+    return projects_to_include
