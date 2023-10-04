63c55b8bcf077e1c056ca1cc6f7a63db8f73d19a
toppy007 <chris_t72@hotmail.com>
2023-10-04 20:09:41 +0100

    changed main profile

--- website/views.py
+++ website/views.py
@@ -203,9 +203,9 @@ def ana_cre_main():
                         loading = session.get('loading', False)
                         
                         return render_template('analysis_create/ana_cre_main.html', user=current_user, sections=sections, analysisResult=first_api_response, input_value=job_ad, loading=loading,)
-    
-                session.clear()
-                return render_template('analysis_create/ana_cre_main.html', user=current_user, sections=sections)
+                
+        session.clear()
+        return render_template('analysis_create/ana_cre_main.html', user=current_user, sections=sections)
     
 @views.route('/results', methods=['GET', 'POST'])
 @login_required
